a02b4388eafefc7c329319b3ba742293aa9e7f6f
parth20jakl <151842474+parth20jakl@users.noreply.github.com>
2024-12-20 08:35:20 +0530

    ooppractical2.cpp

--- ooppractical2.cpp
+++ ooppractical2.cpp
@@ -1,145 +1,127 @@
-#include<iostream>
-#include<string.h>
+# include <iostream>
+# include <string>
 using namespace std;
-
-class StudentData;
+class College;
 class Student
 {
-  string name;
-  int roll_no;
-  string cls;
-  char* division;
-  string dob;
-  char* bloodgroup;
-  static int count;
-
-  public:
-  Student()          
-  {
-    name="";
-    roll_no=0;
-    cls="";
-    division=new char;
-    dob="dd/mm/yyyy";
-    bloodgroup=new char[4];
-  }
-  ~Student()
-  {
-    delete division;
-    delete[] bloodgroup;
-  }
-  static int getCount()
-  {
-    return count;
-  }
-  void getData(StudentData*);
-  void displayData(StudentData*);
+    private:
+	        string name, dOB, bloodGroup, address, licenseNo, telephone;
+	public:
+			Student()
+			{
+				name="",dOB="",bloodGroup="",address="",licenseNo="",telephone="";
+				cout<<"Default Student Constructor called."<<endl;
+			}
+			Student(string n, string dB, string bG, string add, string l ,string tele)
+			{
+				name=n, dOB=dB , bloodGroup=bG , address=add , licenseNo=l, telephone=tele;
+				cout<<"Parametrized Student Constructor called."<<endl;
+			}
+			Student(const Student &stud)
+			{
+				cout<<"Copy Student Constructor called."<<endl;
+			}
+			~Student()
+			{
+				cout<<"Student Destructor called."<<endl;
+			}
+			inline void acceptStudent()
+			{
+				cout<<"Enter your Name : "<<endl;
+				cin>>name;
+				cout<<"Enter your Date of Birth : "<<endl;
+				cin>>dOB;
+				cout<<"Enter your Blood Group : "<<endl;
+				cin>>bloodGroup;
+				cout<<"Enter your Address : "<<endl;
+				cin.get();
+				getline(cin,address);
+				cout<<"Enter your License Number : "<<endl;
+				cin>>licenseNo;
+				cout<<"Enter your Telephone Number : "<<endl;
+				cin>>telephone;
+			}
+			friend class College;
 };
-
-class StudentData
+class College
 {
-  string caddress;
-  long int* telno;
-  long int* dlno;
-  friend class Student;
-
-  public:
-  StudentData()
-  {
-     caddress="";
-     telno=new long;
-     dlno=new long;
-  }
-  ~StudentData()
-  {
-    delete telno;
-    delete dlno;
-  }
-  void getStudentData()
-  {
-     cout<<"Enter Contact Address : ";
-     cin.get();
-     getline(cin,caddress);
-     cout<<"Enter Telephone Number : ";  
-     cin>>*telno;
-     cout<<"Enter Driving License Number : ";
-     cin>>*dlno;
-  }
-  void displayStudentData()
-  {
-     cout<<"Contact Address : "<<caddress<<endl;
-     cout<<"Telephone Number : "<<*telno<<endl;
-     cout<<"Driving License Number : "<<*dlno<<endl;
-  }
+	private:
+		string className;
+		char division;
+		int roll;
+	public:
+			College()
+			{
+				roll=0;
+				cout<<"Default College Constructor called."<<endl;
+			}
+			College(int r, string cN, char div)
+			{
+				roll=r,division = div, className = cN;
+				cout<<"Parametrized College Constructor called."<<endl;
+			}
+			void acceptCollege()
+			{
+				cout<<"Enter your Roll Number : "<<endl;
+				cin>>roll;
+				cout<<"Enter your Class : "<<endl;
+				cin>>className;
+				cout<<"Enter your Division : "<<endl;
+				cin>>division;
+			}
+			void printData(Student &stud)
+			{
+				cout<<"|Name:"<<stud.name;
+				cout<<"|DOB:"<<stud.dOB;
+				cout<<"|Blood Group:"<<stud.bloodGroup;
+				cout<<"|Address:"<<stud.address;
+				cout<<"|License:"<<stud.licenseNo;
+				cout<<"|Telephone:"<<stud.telephone;
+				cout<<"|Roll Number:"<<roll;
+				cout<<"|Class:"<<className;
+				cout<<"|Division:"<<division;
+                cout<<endl;
+			}
+			~College()
+			{
+				cout<<"College Destructor called."<<endl;
+			}
+				
 };
-
-inline void Student::getData(StudentData* st)
-{
-  cout<<"Enter Student Name : ";
-  getline(cin,name);
-  cout<<"Enter Roll Number : ";
-  cin>>roll_no;
-  cout<<"Enter Class : ";
-  cin.get();
-  getline(cin,cls);
-  cout<<"Enter Division : ";
-  cin>>division;
-  cout<<"Enter Date of Birth : ";
-  cin.get();
-  getline(cin,dob);
-  cout<<"Enter Blood Group : ";
-  cin>>bloodgroup;
-  st->getStudentData();
-  count++;
-}
-
-inline void Student::displayData(StudentData* st1)
-{
-  cout<<"Student Name : "<<name<<endl;
-  cout<<"Roll Number : "<<roll_no<<endl;
-  cout<<"Class : "<<cls<<endl;
-  cout<<"Division : "<<division<<endl;
-  cout<<"Date of Birth : "<<dob<<endl;
-  cout<<"Blood Group : "<<bloodgroup<<endl;
-  st1->displayStudentData();
-}
-
-int Student::count;
-
 int main()
 {
-  Student* stud1[100];
-  StudentData* stud2[100];
-  int n=0;
-  char ch;
-
-  do
-  {
-    stud1[n]=new Student;
-    stud2[n]=new StudentData;
-    stud1[n]->getData(stud2[n]);
-    n++;
-    cout<<"Do you want to add another student (y/n) : ";
-    cin>>ch;
-    cin.get();
-  } 
-  while (ch=='y' || ch=='Y');
-  {
-  for(int i=0;i<n;i++)
-   {
-     cout<<"---------------------------------------------------------------"<<endl;
-     stud1[i]->displayData(stud2[i]);
-   } 
-    cout<<"---------------------------------------------------------------"<<endl;
-    cout<<"Total Students : "<<Student::getCount();
-    cout<<endl<<"---------------------------------------------------------------"<<endl;
-
-    for(int i=0;i<n;i++)
-    {
-        delete stud1[i];
-        delete stud2[i];
-    }
-  } 
- cout<<"\nName:- Parth Jayant Kulkarni, Roll.no:SEAD231250, Div:A";
-return 0;
+	Student s1("Sam","1/1/20","AB","KBTCOE","E-10001","0253-2345678");
+	Student s2=s1;
+    Student *s[10];
+    College *c[10];
+    int i =0;
+	while(1)
+	{
+		int ch;
+		cout<<"Enter 1-Enter | 2-Display | 0-exit : "<<endl;
+		cin>>ch;
+		if(ch==1)
+		{	
+			cout<<"**** Accepting New Student -- "<<endl;
+            s[i] = new Student();
+            c[i] = new College();
+			s[i]->acceptStudent();
+			c[i]->acceptCollege();
+			cout<<"**** Printing the Student -- "<<endl;
+			c[i]->printData(*s[i]);
+            i++;
+		}
+        else if(ch==2)
+		{	
+			cout<<"**** Printing the Database -- "<<endl;
+            for(int j=0;j<i;j++)
+			{    c[j]->printData(*s[j]); }
+            
+		}
+		else(ch == 0)
+		{
+			break;
+		}
+	}
+	return 0;
 }
